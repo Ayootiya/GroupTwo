e1c5a9486e666e0de1144dfed0ba9e5b52ae30a5
CodeZola <codezola@gmail.com>
2022-07-22 00:13:34 -0400

    We create React App Happy Coding
    We create React

---
+++ stategroupproject/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Grouptwo from './Componnent/Grouptwo'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+<Grouptwo />
+      </div>
+    )
+  }
+}
+export default App
---
+++ stategroupproject/src/Componnent/Grouptwo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class Grouptwo extends Component {
+  render() {
+    return (
+      <div>
+      <h1>This is wher we put all our componnent</h1>
+      </div>
+    )
+  }
+}
+export default Grouptwo
